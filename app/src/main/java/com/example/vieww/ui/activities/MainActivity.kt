cc7ef70f5bffd31f4ac920014e5b75cebe87d69b
BryanL22 <90713107+BryanL22@users.noreply.github.com>
2021-11-22 16:48:09 -0500

    Archivos faltantes

---
+++ app/src/main/java/com/example/vieww/ui/activities/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.cafesofiareal.vieww.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.cafesofiareal.R
+import com.example.cafesofiareal.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+    }
+
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnvMenu).setupWithNavController(navController)
+    }
+
+}
