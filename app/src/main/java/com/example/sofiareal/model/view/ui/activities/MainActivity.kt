7e5bdc45d922d499e103c8b0faadf2c2d1aa4628
JeissonMS <83564558+JeissonMS@users.noreply.github.com>
2021-11-23 11:20:24 -0500

    https://github.com/BryanL22/MovilApp.git
    first commint guia 9 & 10

---
+++ app/src/main/java/com/example/sofiareal/model/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sofiareal.model.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sofiareal.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
